b500cc99678dddc04cd8f4fd7d41ab768d621e44
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-19 14:19:50 -0500

    Create GradeBookTypes

---
+++ GradeBook/Enums/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.GradeBooks;
+
+namespace GradeBook.Enums
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
